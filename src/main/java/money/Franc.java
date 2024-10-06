4050a9c560ffae0736366122ec434146f5f55a04
inoueshoei <inoueshoei.313.9720@gmail.com>
2024-10-06 16:42:52 +0900

    [Add]第5章追加

---
+++ src/main/java/money/Franc.java
@@ -0,0 +1,17 @@
+package money;
+
+public class Franc {
+    private int amount;
+    public Franc(int amount){
+        this.amount=amount;
+    }
+
+    Franc times(int multiplier){
+        return new Franc(amount*multiplier);
+    }
+
+    public boolean equals(Object object){
+        Franc dollar = (Franc) object;
+        return amount==dollar.amount;
+    }
+}
